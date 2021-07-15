b496d36b24d7dcf28d224008017da2dd8da3b69b
responsaeuris <responsaeuris@gmail.com>
2021-07-15 10:24:15 +0000

    d38e2077cb3dbb9135f3691ba815d9a03766391d chore_ upgrade version to 0.2.5 CI fingerprint

--- app/routes/status/statusRoute.js
+++ app/routes/status/statusRoute.js
@@ -1,6 +1,6 @@
 const status = async () => {
-  const ciCommit = 'CI_PUTS_HERE_LAST_GIT_COMMIT'
-  const lastDeploy = 'CI_PUTS_HERE_DEPLOY_DATE'
+  const ciCommit = 'd38e2077cb3dbb9135f3691ba815d9a03766391d chore_ upgrade version to 0.2.5'
+  const lastDeploy = 'Thu Jul 15 10:23:30 UTC 2021'
 
   return `ok! Plugin Core released on ${lastDeploy}, last commit was "${ciCommit}"`
 }
