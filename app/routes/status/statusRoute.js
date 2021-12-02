b226849749ccbcba5f9993e8f3471553519c1c52
responsaeuris <responsaeuris@gmail.com>
2021-12-02 16:07:45 +0000

    a72ef81e0afb29e957ccbe373334d64a0a2caa16 Merge tag _0.2.6_ into develop CI fingerprint

--- app/routes/status/statusRoute.js
+++ app/routes/status/statusRoute.js
@@ -1,6 +1,6 @@
 const status = async () => {
-  const ciCommit = 'CI_PUTS_HERE_LAST_GIT_COMMIT'
-  const lastDeploy = 'CI_PUTS_HERE_DEPLOY_DATE'
+  const ciCommit = 'a72ef81e0afb29e957ccbe373334d64a0a2caa16 Merge tag _0.2.6_ into develop'
+  const lastDeploy = 'Thu Dec  2 16:07:04 UTC 2021'
 
   return `ok! Plugin Core released on ${lastDeploy}, last commit was "${ciCommit}"`
 }
